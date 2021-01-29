b5466359e1719c090121e11b9e07bcc5728365a4
ji21 <jeffreyip54@gmail.com>
2021-01-29 18:10:47 +0000

    included fragments

--- app/src/main/java/com/example/karat/MainActivity.kt
+++ app/src/main/java/com/example/karat/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.karat
 
+import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.TranslateAnimation
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.core.view.MotionEventCompat
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.karat.databinding.ActivityMainBinding
@@ -16,6 +23,7 @@ import com.example.karat.fragments.NewsFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var opened = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         configureBottomNav()
         configureTopAppBar()
+        configureNavDrawer()
+    }
+
+
+    private fun configureNavDrawer() {
+        binding.drawerLayout.isInvisible = true
     }
 
     private fun configureBottomNav() {
@@ -45,17 +59,43 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply{
-            replace(R.id.screen, fragment)
+            replace(R.id.fragment, fragment)
             commit()
         }
 
     private fun configureTopAppBar() {
         binding.profile.setBackgroundResource(R.drawable.ic_placeholder)
-        binding.profile.setOnClickListener {
-            println("owieurhgjw")
-            Log.d("TAG", "message")
+    }
+
+    fun openCloseNavigationDrawer(view: View) {
+        val offset = binding.drawerLayout.width.toFloat()
+        if (opened) {
+            binding.profile.alpha = 1.0f
+            binding.drawerLayout.isInvisible = true
+            moveRootView(0f)
+        } else {
+            binding.profile.alpha = 0f
+            animateOpenDrawer()
+            moveRootView(-offset)
         }
-        Log.d("TAG", "message")
+        opened = !opened
+    }
+
+    private fun animateOpenDrawer() {
+        val drawer = binding.drawerLayout
+        val profile = binding.profile
+        drawer.isInvisible = false
+        drawer.alpha = 0f
+        drawer.animate().duration = 500
+        drawer.animate().alpha(1f).start()
+
+    }
+
+    private fun moveRootView(offset: Float) {
+        ObjectAnimator.ofFloat(binding.screen, "translationX", offset).apply {
+            duration = 100
+            start()
+        }
     }
 
 }
